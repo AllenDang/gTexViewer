34ee8ecf68efe5406170ae0259e840368198e1b4
Allen <allengnr@gmail.com>
2025-08-12 16:35:50 +0800

    Add release github ci

---
+++ build.rs
@@ -0,0 +1,11 @@
+#[cfg(target_os = "windows")]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/icon/app.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+    // Do nothing for non-Windows platforms
+}
